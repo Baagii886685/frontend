24a399dca456b04ebe3790f797e27273b9d2173c
Batgerel <Batgerelbaagii11@gmail.com>
2023-12-19 19:23:47 +0800

    asdfdf

--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -3,7 +3,7 @@
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque animi quia impedit, repudiandae expedita in quas
       quidem asperiores molestiae reprehenderit corrupti suscipit quae ratione. Non officiis ipsam corrupti rem
-      quibusdam.
+      quibusdamsdfgsdfg.
     </p>
   </div>
 </template>
